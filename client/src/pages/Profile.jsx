679f68c6ac80a931da884f4e6d64a394e7bacfdf
tushareshukla <bornlibra04@gmail.com>
2023-10-22 17:12:03 +0530

    sign out function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
+  signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 export default function Profile() {
@@ -96,6 +97,20 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -165,7 +180,9 @@ export default function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign Out
+        </span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -43,11 +43,25 @@ const userSlice = createSlice({
         deleteUserFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
+            
+        },
+        signOutUserStart:(state) => {
+            state.loading = true;
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        signOutUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+            
         },
         }
     },
 );
-export const { signInStart, signInSuccess, signInFailure,updateUserFailure,updateUserStart,updateUserSuccess,
+export const { signOutUserStart, signOutUserFailure, signOutSuccess,signInStart, signInSuccess, signInFailure,updateUserFailure,updateUserStart,updateUserSuccess,
 deleteUserFailure,
 deleteUserSuccess, deleteUserStart} = userSlice.actions;
 
