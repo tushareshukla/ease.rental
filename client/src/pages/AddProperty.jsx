5d7180c25e41925a740555bcbb31b7f198d25c07
tushareshukla <bornlibra04@gmail.com>
2023-10-23 01:19:06 +0530

    PAST

--- client/src/pages/AddProperty.jsx
+++ client/src/pages/AddProperty.jsx
@@ -1,23 +1,41 @@
-import { useState, useEffect } from "react";
-import { app } from "../firebase";
+import { useState } from "react";
 import {
+  getDownloadURL,
   getStorage,
   ref,
   uploadBytesResumable,
-  getDownloadURL,
 } from "firebase/storage";
-import { set } from "mongoose";
+import { app } from "../firebase";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 export default function AddProperty() {
-  const [uploading, setUploading] = useState(false);
-  const [imageUploadError, setImageUploadError] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
-  const [city, setCity] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [address, setAddress] = useState("");
   const [formData, setFormData] = useState({
     imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "rent",
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
+    temple: false,
+    garden: false,
+    city: "",
+    pool: false,
+    area: "",
   });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log(formData);
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
@@ -37,7 +55,7 @@ export default function AddProperty() {
           setUploading(false);
         })
         .catch((err) => {
-          setImageUploadError("Image upload failed (2 mb max per image");
+          setImageUploadError("Image upload failed (2 mb max per image)");
           setUploading(false);
         });
     } else {
@@ -56,7 +74,7 @@ export default function AddProperty() {
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`upload is ${progress}% done`);
+          console.log(`Upload is ${progress}% done`);
         },
         (error) => {
           reject(error);
@@ -69,53 +87,6 @@ export default function AddProperty() {
       );
     });
   };
-
-  // Function to fetch and update the user's city based on geolocation
-  const detectCity = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const { latitude, longitude } = position.coords;
-
-        // Use the Google Maps Geocoding API to determine the city
-        const detectedCity = await determineCityByCoordinates(
-          latitude,
-          longitude
-        );
-
-        setCity(detectedCity);
-      });
-    }
-  };
-
-  // Function to determine the city based on coordinates using Google Maps Geocoding API
-  const determineCityByCoordinates = async (latitude, longitude) => {
-    try {
-      const apiKey = "YOUR_GOOGLE_MAPS_API_KEY";
-      const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
-      );
-      const data = await response.json();
-
-      // Extract the city from the geocoding results (you may need to adjust this based on the API response)
-      const cityResult = data.results.find((result) => {
-        return result.types.includes("locality");
-      });
-
-      if (cityResult) {
-        return cityResult.formatted_address;
-      } else {
-        return "Unknown City";
-      }
-    } catch (error) {
-      console.error("Error determining city:", error);
-      return "Unknown City";
-    }
-  };
-
-  useEffect(() => {
-    // Auto-detect the user's city on component load
-    detectCity();
-  }, []);
   const handleRemoveImage = (index) => {
     setFormData({
       ...formData,
@@ -123,83 +94,201 @@ export default function AddProperty() {
     });
   };
 
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "pool" ||
+      e.target.id === "temple" ||
+      e.target.id === "garden" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload at least one image");
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError("Discount price must be lower than regular price");
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
-      <h1 className="text-center text-3xl font-semibold my-7">Add Property</h1>
-      <form className="flex flex-col sm:flex-row gap-4">
+      <h1 className="text-3xl font-semibold text-center my-7">Add Property</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
             type="text"
-            placeholder="Add your property to ease.rentals"
+            placeholder="Add your property"
             className="border p-3 rounded-lg"
             id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
             maxLength="62"
             minLength="10"
             required
+            onChange={handleChange}
+            value={formData.name}
           />
           <textarea
-            placeholder="Description-(eg: sq.feet area, nearby prime spots, distance from near by railway, Muslims not allowed)"
+            type="text"
+            placeholder="Description-(Eg. Nearbly location, rail-distance, amenities, Muslims allowed or not , bachelors, Sq. feet )"
             className="border p-3 rounded-lg"
             id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
             required
+            onChange={handleChange}
+            value={formData.description}
           />
           <input
             type="text"
-            placeholder="Addres, Area, Road, Landmark"
+            placeholder="Address, Room No., Plot Number, Survey No."
             className="border p-3 rounded-lg"
             id="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
             required
+            onChange={handleChange}
+            value={formData.address}
           />
           <input
             type="text"
-            placeholder="City"
+            placeholder="Area, landmark, Road"
+            className="border p-3 rounded-lg"
+            id="area"
+            required
+            onChange={handleChange}
+            value={formData.area}
+          />
+          <input
+            type="text"
+            placeholder="City-(Eg: Vapi, Daman, Surat)"
             className="border p-3 rounded-lg"
             id="city"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
             required
+            onChange={handleChange}
+            value={formData.city}
           />
-          <div className=" flex gap-6 flex-wrap">
+          <div className="flex gap-6 flex-wrap">
             <div className="flex gap-2">
-              <input type="checkbox" id="sell" className="w-5" />
+              <input
+                type="checkbox"
+                id="sale"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.type === "sale"}
+              />
               <span>Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" className="w-5" />
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.type === "rent"}
+              />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="carparking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.parking}
+              />
               <span>Car Parking</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
-              <span>Furnished</span>
-            </div>
-            <div className="flex gap-2">
-              <input type="checkbox" id="garden" className="w-5" />
-              <span>Garden</span>
-            </div>
-            <div className="flex gap-2">
-              <input type="checkbox" id="clubhouse" className="w-5" />
-              <span>Club House</span>
-            </div>
-            <div className="flex gap-2">
-              <input type="checkbox" id="pool" className="w-5" />
-              <span>Swimming Pool</span>
-            </div>
-            <div className="flex gap-2">
-              <input type="checkbox" id="temple" className="w-5" />
+              <input
+                type="checkbox"
+                id="temple"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.temple}
+              />
               <span>Temple</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="pool"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.pool}
+              />
+              <span>Swimming Pool</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.furnished}
+              />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="garden"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.garden}
+              />
+              <span>Garden</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.offer}
+              />
               <span>Offer</span>
             </div>
           </div>
@@ -212,55 +301,65 @@ export default function AddProperty() {
                 max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
+                onChange={handleChange}
+                value={formData.bedrooms}
               />
-              <p>Bed Room</p>
+              <p>Beds</p>
             </div>
             <div className="flex items-center gap-2">
               <input
                 type="number"
-                id="washroom"
+                id="bathrooms"
                 min="1"
                 max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
+                onChange={handleChange}
+                value={formData.bathrooms}
               />
-              <p>Washroom</p>
+              <p>Baths</p>
             </div>
             <div className="flex items-center gap-2">
               <input
                 type="number"
-                id="regularprice"
-                min="1"
-                max="1000000"
+                id="regularPrice"
+                min="50"
+                max="10000000"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
+                onChange={handleChange}
+                value={formData.regularPrice}
               />
               <div className="flex flex-col items-center">
-                <p>Regular Price</p>
-                <span className="text-xs">(₹/month)</span>
+                <p>Regular price</p>
+                <span className="text-xs">(₹ / month)</span>
               </div>
             </div>
-            <div className="flex items-center gap-2">
-              <input
-                type="number"
-                id="discountedprice"
-                min="1"
-                max="1000000"
-                required
-                className="p-3 border border-gray-300 rounded-lg"
-              />
-              <div className="flex flex-col items-center">
-                <p>Discounted Price</p>
-                <span className="text-xs">(₹/month)</span>
+            {formData.offer && (
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  id="discountPrice"
+                  min="0"
+                  max="10000000"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.discountPrice}
+                />
+                <div className="flex flex-col items-center">
+                  <p>Discounted price</p>
+                  <span className="text-xs">(₹ / month)</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
           <p className="font-semibold">
-            Images:-
-            <span className="font-normal text-gray-500 ml-2">
-              Images of property eg: rooms, locality max.6
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first image will be the cover (max 6)
             </span>
           </p>
           <div className="flex gap-4">
@@ -268,7 +367,7 @@ export default function AddProperty() {
               onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
-              id="images1"
+              id="images"
               accept="image/*"
               multiple
             />
@@ -276,7 +375,7 @@ export default function AddProperty() {
               type="button"
               disabled={uploading}
               onClick={handleImageSubmit}
-              className="p-3 text-green-800 border border-green-800 rounded uppercase hover:shadow-lg disabled:opacity-70"
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
             >
               {uploading ? "Uploading..." : "Upload"}
             </button>
@@ -286,7 +385,10 @@ export default function AddProperty() {
           </p>
           {formData.imageUrls.length > 0 &&
             formData.imageUrls.map((url, index) => (
-              <div className="flex justify-between p-3 border item-center">
+              <div
+                key={url}
+                className="flex justify-between p-3 border items-center"
+              >
                 <img
                   src={url}
                   alt="listing image"
@@ -295,15 +397,19 @@ export default function AddProperty() {
                 <button
                   type="button"
                   onClick={() => handleRemoveImage(index)}
-                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-65"
+                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
                 >
                   Delete
                 </button>
               </div>
             ))}
-          <button className="p-3 bg-gray-700 text-white rounded-lg uppercase hover:opacity-80 disabled:opacity-70">
-            LIST PROPERTY
+          <button
+            disabled={loading || uploading}
+            className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Adding Listing..." : "ADD listing"}
           </button>
+          {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
       </form>
     </main>
