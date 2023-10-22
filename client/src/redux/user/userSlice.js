22455371b1328717c01bd5d883134897f2b695ce
tushareshukla <bornlibra04@gmail.com>
2023-10-22 16:52:22 +0530

    delete user funcitonality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -28,13 +28,27 @@ const userSlice = createSlice({
             state.currentUser = action.payload;
             state.loading = false;
             state.error = null;},
-       updateUserFailure:(state,action)=> {
+        updateUserFailure:(state,action)=> {
                 state.error = action.payload;
                 state.loading= false;
-            }
+            },
+        deleteUserStart:(state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
         }
-    }
+    },
 );
-export const { signInStart, signInSuccess, signInFailure,updateUserFailure,updateUserStart,updateUserSuccess} = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure,updateUserFailure,updateUserStart,updateUserSuccess,
+deleteUserFailure,
+deleteUserSuccess, deleteUserStart} = userSlice.actions;
 
 export default userSlice.reducer;
